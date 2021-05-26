5003b4fe9394cf4ffe80503466fd51ec814945a6
Xiaowei <xiaowei.c.li@gmail.com>
2021-05-26 16:55:54 +0000

    example of while loop with an if loop in it

---
+++ chorus-while.js
@@ -0,0 +1,14 @@
+//EXAMPLE 1
+
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+} 
+console.log("Until the sun comes up!");
+
+
