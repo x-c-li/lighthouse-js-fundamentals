48d6ed625176ce376d1e6b7a0c8ab1a9808a9f25
Xiaowei <xiaowei.c.li@gmail.com>
2021-05-29 17:10:12 +0000

    small test in arrays

---
+++ ice-cream.js
@@ -0,0 +1,22 @@
+const iceCreamFlavours = ["chocolate", 
+"vanilla", 
+"cookies and cream", 
+"rocky road", 
+"strawberry"];
+
+console.log(iceCreamFlavours);
+
+
+//STEP 1 (add root beer without changing the list)
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//STEP 2 (get the first flavour) 
+console.log(iceCreamFlavours[0]);
+
+//STEP 2 (get the last flavour in the array) 
+console.log(iceCreamFlavours[5]); 
+
+//STEP 3 (log total number of flavours) 
+console.log(iceCreamFlavours.length);
+  
